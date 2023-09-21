be2e8b67704c2a2339ef5fe1a5585861f7a6ada0
Daimar <alex321solano@gmail.com>
2023-09-21 16:36:31 -0500

    conexion de base de datos consulta

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>php</title>
 </head>
 <body>
+
 <?php
     require_once __DIR__ . "/vendor/autoload.php";
 
@@ -15,23 +16,12 @@
 
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
+    $nombre = "test desde php";
+    require_once __DIR__ . "\conexion.php";
+    echo $_ENV["DATABASE"];
 
-    echo $_ENV["SERVER"];
-    ?>
-    
-     <!-- try {
-        $sentencia = $mysqli->prepare("INSERT INTO gender (name) VALUES (?)");
-        $sentencia->bind_param("s", $nombre);
-        echo  $sentencia->execute();
-    } catch (\Throwable $th) {
-        echo $th;
-    }  -->
+?>
 
-    <form action="" method="get">
-        <input type="text" name="name" id="">
-        <input type="submit" value="Value">
-    </form>
-  
 
     
 </body>
